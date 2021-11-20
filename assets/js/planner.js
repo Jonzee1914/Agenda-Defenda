b82b0b0893913a641e6e506c35c3d9e8028fce70
Erik Jones <jonzee1914@gmail.com>
2021-11-20 17:03:54 -0500

    added color coding for timeblocks

--- assets/js/planner.js
+++ assets/js/planner.js
@@ -1,3 +1,6 @@
+var mjs = moment();
+var currentHour = mjs.hours();
+
 // Set current date and current time
 moment(Date);
 $("#currentDay").text(moment().format('dddd MMMM Do YYYY, h:mm a'));
@@ -5,27 +8,48 @@ $("#currentDay").text(moment().format('dddd MMMM Do YYYY, h:mm a'));
 var currentTime = moment();
 // Shift time to the top of the hour e.g. 8:25 becomes 8
 currentTime = currentTime.startOf("hour");
-// Pushes day start by + 8 to 8 am
-var beforeTime = moment().startOf('day').add(8, "hours");
+
+// Array for agenda
+var agendaItems = [];
 
 // Set hours of each timeblock to a moment object and displays in designated format
 var time0 = currentTime.hour(8).format("hh:mm A");
-$(".block8").text(time0);
+$(".block-8").text(time0);
 var time1 = currentTime.hour(9).format("hh:mm A");
-$(".block9").text(time1);
+$(".block-9").text(time1);
 var time2 = currentTime.hour(10).format("hh:mm A");
-$(".block10").text(time2);
+$(".block-10").text(time2);
 var time3 = currentTime.hour(11).format("hh:mm A");
-$(".block11").text(time3);
+$(".block-11").text(time3);
 var time4 = currentTime.hour(12).format("hh:mm A");
-$(".block12").text(time4);
+$(".block-12").text(time4);
 var time5 = currentTime.hour(13).format("hh:mm A");
-$(".block1").text(time5);
+$(".block-1").text(time5);
 var time6 = currentTime.hour(14).format("hh:mm A");
-$(".block2").text(time6);
+$(".block-2").text(time6)
 var time7 = currentTime.hour(15).format("hh:mm A");
-$(".block3").text(time7);
+$(".block-3").text(time7);
 var time8 = currentTime.hour(16).format("hh:mm A");
-$(".block4").text(time8);
+$(".block-4").text(time8);
 var time9 = currentTime.hour(17).format("hh:mm A");
-$(".block5").text(time9);
+$(".block-5").text(time9);
+
+// Compare current time to timeblock
+initializeAgenda();
+
+function initializeAgenda() {
+
+    $("input[type=text]").each(function (){
+        var hourChoices = parseInt($(this).attr("id").split("-")[1]);
+            if (hourChoices < currentHour) {
+                $(this).addClass("past");
+                $(this).removeClass("present", "future");
+            } else if (hourChoices === currentHour) {
+                $(this).addClass("present");
+                $(this).removeClass("past", "future");
+            } else {
+                $(this).addClass("future");
+                $(this).removeClass("past", "present");
+            };
+    });
+};
